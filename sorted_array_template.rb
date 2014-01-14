e59351fd10832f685b32e38cd708565567603886
afleisch <afleisch@gmail.com>
2014-01-13 20:15:50 -0800

    first commit

--- sorted_array_template.rb
+++ sorted_array_template.rb
@@ -3,12 +3,15 @@ class SortedArray
 
   def initialize(input_arr=[])
     @internal_arr = []
+    input_arr.each{|elem| self.add(elem)}
+
     # Fill in the rest of the initialize method here.
     # What should you do with each element of the incoming array?
   end
 
-  def add(new_ele)
+  def add(new_elem)
     # Hint: Use the Array#insert method.
+    @internal_arr.insert(first_larger_index(new_elem), 0, @internal_arr.size),
     raise NotImplementedError.new("You need to implement the add method!")
   end
 
@@ -21,7 +24,17 @@ class SortedArray
   end
 
   def first_larger_index(target, start_ind=0, end_ind=@internal_arr.size)
-    raise NotImplementedError.new("You need to implement the first_larger_index method!")
+    if start_ind >= end_ind then return 0 end
+    if (end_ind+start_ind) ==1
+      if target < @internal_arr[start_ind]
+        return start_ind
+      else return end_ind
+      end
+    end
+
+    mid_index = (start_ind + end_ind) / 2
+    if target 
+    # raise NotImplementedError.new("You need to implement the first_larger_index method!")
   end
 
   def index(target)
